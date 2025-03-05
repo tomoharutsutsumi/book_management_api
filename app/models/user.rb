341c15b0721c4cb492fdbdc50f1af7e108b4ad98
tomoharutsutsumi <ttomoharu201@gmail.com>
2025-03-04 20:59:59 -0800

    add validation user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   before_create :generate_account_number
 
+  validates :account_number, presence: true, uniqueness: true
+  validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   private
 
   def generate_account_number
